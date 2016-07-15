5d70dd6d95d089eb6239dce789e66c48eb31837d
sandeep <sandeep@sandeep-Lenovo-Z710>
2016-07-14 22:43:55 -0500

    Quick Sort Updated

--- src/TestCases/TQuickStart.java
+++ src/TestCases/TQuickStart.java
@@ -8,9 +8,10 @@ public class TQuickStart {
 
 	public static void main(String[] args) {
 		int[] intArr = {2,7,2,9,0,5,3,4,5,8};
+		System.out.println("Before Sort: "+Arrays.toString(intArr));
 		intArr = NSquareSort.removeDuplicate(intArr);
 		intArr = NSquareSort.findNSquareSort(intArr);
-		System.out.println(Arrays.toString(intArr));
+		System.out.println("After Sort: "+Arrays.toString(intArr));
 	}
 
 }
